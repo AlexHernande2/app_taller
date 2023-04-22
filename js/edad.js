c83efca5407543bec0470c1e8b3e4f2fe52b4221
AlexHernande2 <edalhernandez@uniboyaca.edu.co>
2023-04-22 16:14:30 -0500

    Se hacen correcciones en el código 

--- js/edad.js
+++ js/edad.js
@@ -1,11 +1,13 @@
 function mayorEdad() {
-    const edad = document.getElementById('edad').value;
+    const edad = parseFloat(document.getElementById('edad').value);
     const nombre = document.getElementById('nombre').value;
     if (nombre.trim() == '') {
         alert('Debe ingresar un nombre');
     } else {
         if (!edad) {
             alert('Debe ingresar una edad');
+        } else if (!Number.isInteger(edad)) {
+            alert('Deben ser números enteros');
         } else if (edad >= 18 && edad <= 120) {
             alert('Hola ' + nombre + ' Eres mayor de Edad');
         } else if (edad > 0 && edad <= 17) {
